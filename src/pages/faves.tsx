d79b38d7895296e6bee98fdcd3d4fb0c8a80a67f
Facundo Prieto <facundo.prieto321@gmail.com>
2021-09-17 17:53:26 -0300

    Create pages with mock data

--- src/pages/faves.tsx
+++ src/pages/faves.tsx
@@ -2,40 +2,17 @@ import { useRouter } from "next/dist/client/router";
 import { Card } from "src/components/Card";
 import { Header } from "src/components/Header";
 import { NavigationSwitch } from "src/components/NavigationSwitch";
-import { Select } from "src/components/Select";
+
 import styles from "../styles/pageStyles.module.css";
 
-export const Home = (): JSX.Element => {
+export const Faves = (): JSX.Element => {
   const router = useRouter();
   return (
-    <div className={styles.pageContainer}>
+    <div>
       <Header />
       <div className={styles.contentContainer}>
         <div className={styles.navigation}>
           <NavigationSwitch actualPage={router.asPath} />
-        </div>
-        <div className={styles.selectNews}>
-          <Select
-            title="Select your news"
-            items={[
-              {
-                src: "/icon-angular.png",
-                name: "Angular",
-                value: "angular",
-              },
-              {
-                src: "/icon-react.png",
-                name: "React",
-                value: "reactjs",
-              },
-              {
-                src: "/icon-vuejs.png",
-                name: "VueJs",
-                value: "vuejs",
-              },
-            ]}
-            handleSelection={(value) => alert(value)}
-          />
         </div>
         <div className={styles.cardsContainer}>
           <Card
@@ -46,7 +23,7 @@ export const Home = (): JSX.Element => {
             }
             story_url="http://ycombinator.com"
             created_at="2021-09-16T18:21:51.000Z"
-            favorited={false}
+            favorited={true}
           />
           <Card
             id={1}
@@ -56,7 +33,7 @@ export const Home = (): JSX.Element => {
             }
             story_url={"http://ycombinator.com"}
             created_at={"2021-09-16T18:21:51.000Z"}
-            favorited={false}
+            favorited={true}
           />
           <Card
             id={1}
@@ -66,7 +43,7 @@ export const Home = (): JSX.Element => {
             }
             story_url={"http://ycombinator.com"}
             created_at={"2021-09-16T18:21:51.000Z"}
-            favorited={false}
+            favorited={true}
           />
           <Card
             id={1}
@@ -76,7 +53,7 @@ export const Home = (): JSX.Element => {
             }
             story_url={"http://ycombinator.com"}
             created_at={"2021-09-16T18:21:51.000Z"}
-            favorited={false}
+            favorited={true}
           />
           <Card
             id={1}
@@ -86,37 +63,7 @@ export const Home = (): JSX.Element => {
             }
             story_url={"http://ycombinator.com"}
             created_at={"2021-09-16T18:21:51.000Z"}
-            favorited={false}
-          />
-          <Card
-            id={1}
-            author={"author"}
-            story_title={
-              "Yes, React is taking over front-end development. The question is why."
-            }
-            story_url={"http://ycombinator.com"}
-            created_at={"2021-09-16T18:21:51.000Z"}
-            favorited={false}
-          />
-          <Card
-            id={1}
-            author={"author"}
-            story_title={
-              "Yes, React is taking over front-end development. The question is why."
-            }
-            story_url={"http://ycombinator.com"}
-            created_at={"2021-09-16T18:21:51.000Z"}
-            favorited={false}
-          />
-          <Card
-            id={1}
-            author={"author"}
-            story_title={
-              "Yes, React is taking over front-end development. The question is why."
-            }
-            story_url={"http://ycombinator.com"}
-            created_at={"2021-09-16T18:21:51.000Z"}
-            favorited={false}
+            favorited={true}
           />
         </div>
       </div>
@@ -124,4 +71,4 @@ export const Home = (): JSX.Element => {
   );
 };
 
-export default Home;
+export default Faves;
